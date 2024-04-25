985b9198d5559fda5e7df86da8189d67146a68ca
Akshay Vr <avr02339@gmail.com>
2024-04-25 14:50:00 +0530

    All product Api added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const {addProduct,  getCategory}=require('../controllers/productController')
+const {addProduct,  getCategory, getAllProduct}=require('../controllers/productController')
 
 
 const router=express.Router()
@@ -7,6 +7,7 @@ const router=express.Router()
 router.route('/').post(addProduct)
 // router.route('/categorynames').get(getCategoryNames)
 router.route('/').get(getCategory)
+router.route('/allproduct').get(getAllProduct)
 
 
 
